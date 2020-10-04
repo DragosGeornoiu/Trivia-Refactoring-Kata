5a157a68e2a2740c0de22fbedf9d5850b6c237a2
Dragos Geornoiu <dgeornoiu@luxoft.com>
2020-10-04 18:52:29 +0300

    Initial code.

--- src/main/java/ro/dragos/geornoiu/Game.java
+++ src/main/java/ro/dragos/geornoiu/Game.java
@@ -10,7 +10,13 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+
 public class Game implements IGame {
+
+    //~ ----------------------------------------------------------------------------------------------------------------
+    //~ Instance fields 
+    //~ ----------------------------------------------------------------------------------------------------------------
+
     private List<Player> players = new ArrayList<>();
     int[] purses = new int[6];
     boolean[] inPenaltyBox = new boolean[6];
@@ -23,6 +29,10 @@ public class Game implements IGame {
     int currentPlayer = 0;
     boolean isGettingOutOfPenaltyBox;
 
+    //~ ----------------------------------------------------------------------------------------------------------------
+    //~ Constructors 
+    //~ ----------------------------------------------------------------------------------------------------------------
+
     public Game() {
         for (int i = 0; i < 50; i++) {
             popQuestions.add("Pop Question " + i);
@@ -31,6 +41,10 @@ public class Game implements IGame {
             rockQuestions.add(createRockQuestion(i));
         }
     }
+
+    //~ ----------------------------------------------------------------------------------------------------------------
+    //~ Methods 
+    //~ ----------------------------------------------------------------------------------------------------------------
 
     public String createRockQuestion(int index) {
         return "Rock Question " + index;
@@ -55,15 +69,14 @@ public class Game implements IGame {
         System.out.println("They have rolled a " + roll);
 
         if (inPenaltyBox[currentPlayer]) {
-            if (roll % 2 != 0) {
+            if ((roll % 2) != 0) {
                 isGettingOutOfPenaltyBox = true;
 
                 System.out.println(currentPlayer().name() + " is getting out of the penalty box");
                 movePlayer(roll);
 
-                System.out.println(currentPlayer().name()
-                        + "'s new location is "
-                        + currentPlayer().place());
+                System.out.println(currentPlayer().name() +
+                    "'s new location is " + currentPlayer().place());
                 System.out.println("The category is " + currentCategory());
                 askQuestion();
             } else {
@@ -73,49 +86,12 @@ public class Game implements IGame {
 
         } else {
             movePlayer(roll);
-            System.out.println(currentPlayer().name()
-                    + "'s new location is "
-                    + currentPlayer().place());
+            System.out.println(currentPlayer().name() +
+                "'s new location is " + currentPlayer().place());
             System.out.println("The category is " + currentCategory());
             askQuestion();
         }
 
-    }
-
-    private void movePlayer(int roll) {
-        currentPlayer().move(roll);
-    }
-
-    private void askQuestion() {
-        System.out.println(extractNextQuestion());
-    }
-
-    private String extractNextQuestion() {
-        return switch (currentCategory()) {
-            case "Pop" -> popQuestions.remove(0);
-            case "Science" -> scienceQuestions.remove(0);
-            case "Sports" -> sportsQuestions.remove(0);
-            case "Rock" -> rockQuestions.remove(0);
-            default -> throw new IllegalStateException("Unexpected value: " + currentCategory());
-        };
-    }
-
-
-    private String currentCategory() {
-        if (currentPlayer().place() == 0) return "Pop";
-        if (currentPlayer().place() == 4) return "Pop";
-        if (currentPlayer().place() == 8) return "Pop";
-        if (currentPlayer().place() == 1) return "Science";
-        if (currentPlayer().place() == 5) return "Science";
-        if (currentPlayer().place() == 9) return "Science";
-        if (currentPlayer().place() == 2) return "Sports";
-        if (currentPlayer().place() == 6) return "Sports";
-        if (currentPlayer().place() == 10) return "Sports";
-        return "Rock";
-    }
-
-    private Player currentPlayer() {
-        return players.get(currentPlayer);
     }
 
     public boolean wasCorrectlyAnswered() {
@@ -123,35 +99,35 @@ public class Game implements IGame {
             if (isGettingOutOfPenaltyBox) {
                 System.out.println("Answer was correct!!!!");
                 purses[currentPlayer]++;
-                System.out.println(currentPlayer().name()
-                        + " now has "
-                        + purses[currentPlayer]
-                        + " Gold Coins.");
+                System.out.println(currentPlayer().name() +
+                    " now has " + purses[currentPlayer] +
+                    " Gold Coins.");
 
                 boolean winner = didPlayerWin();
                 currentPlayer++;
-                if (currentPlayer == players.size()) currentPlayer = 0;
+                if (currentPlayer == players.size())
+                    currentPlayer = 0;
 
                 return winner;
             } else {
                 currentPlayer++;
-                if (currentPlayer == players.size()) currentPlayer = 0;
+                if (currentPlayer == players.size())
+                    currentPlayer = 0;
                 return true;
             }
-
 
         } else {
 
             System.out.println("Answer was corrent!!!!");
             purses[currentPlayer]++;
-            System.out.println(currentPlayer().name()
-                    + " now has "
-                    + purses[currentPlayer]
-                    + " Gold Coins.");
+            System.out.println(currentPlayer().name() +
+                " now has " + purses[currentPlayer] +
+                " Gold Coins.");
 
             boolean winner = didPlayerWin();
             currentPlayer++;
-            if (currentPlayer == players.size()) currentPlayer = 0;
+            if (currentPlayer == players.size())
+                currentPlayer = 0;
 
             return winner;
         }
@@ -163,16 +139,60 @@ public class Game implements IGame {
         inPenaltyBox[currentPlayer] = true;
 
         currentPlayer++;
-        if (currentPlayer == players.size()) currentPlayer = 0;
+        if (currentPlayer == players.size())
+            currentPlayer = 0;
         return true;
     }
 
+    private void movePlayer(int roll) {
+        currentPlayer().move(roll);
+    }
+
+    private void askQuestion() {
+        String currentCategory = currentCategory();
+        String question = "";
+        if (currentCategory.equals("Pop")) {
+            question = popQuestions.remove(0);
+        } else if (currentCategory.equals("Science")) {
+            question = scienceQuestions.remove(0);
+        } else if (currentCategory.equals("Sports")) {
+            question = sportsQuestions.remove(0);
+        } else if (currentCategory.equals("Rock")) {
+            question = rockQuestions.remove(0);
+        }
+        System.out.println(question);
+    }
+
+    private String currentCategory() {
+        if (currentPlayer().place() == 0)
+            return "Pop";
+        if (currentPlayer().place() == 4)
+            return "Pop";
+        if (currentPlayer().place() == 8)
+            return "Pop";
+        if (currentPlayer().place() == 1)
+            return "Science";
+        if (currentPlayer().place() == 5)
+            return "Science";
+        if (currentPlayer().place() == 9)
+            return "Science";
+        if (currentPlayer().place() == 2)
+            return "Sports";
+        if (currentPlayer().place() == 6)
+            return "Sports";
+        if (currentPlayer().place() == 10)
+            return "Sports";
+        return "Rock";
+    }
+
+    private Player currentPlayer() {
+        return players.get(currentPlayer);
+    }
 
     private boolean didPlayerWin() {
         return !(purses[currentPlayer] == 6);
     }
 }
-
 
 /**
  * extract method (replace duplicate code)
